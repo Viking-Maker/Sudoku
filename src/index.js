be3a74a286467baf2f96324c569e8ed354ee5d92
Viking-Maker <143838230+Viking-Maker@users.noreply.github.com>
2025-04-08 12:55:03 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 // If you have a main CSS file (optional, but common)
 // import './index.css';
 import App from './App'; // Imports your main App component
-import reportWebVitals from './reportWebVitals'; // Optional performance reporting
+//import reportWebVitals from './reportWebVitals'; // Optional performance reporting
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,4 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
